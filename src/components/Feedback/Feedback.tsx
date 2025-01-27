54a370ce8bee7af1ba7612bcde1295ffabb510db
Anastasiia Yemets <anastasiaemec40@gmail.com>
2025-01-27 09:48:35 +0100

    feedbackChanges

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,56 +1,43 @@
-import Button from '../Button/Button';
-import './styles.css';
-import { useState } from 'react';
+import Button from "../Button/Button";
+import "./styles.css";
+import { useState } from "react";
 
 function Feedback() {
+  const [like, setLikeCount] = useState<number>(0);
+  const [dislike, setDislikeCount] = useState<number>(0);
 
-    const [like, setLikeCount] = useState(0);
-    const [dislike, setDislikeCount] = useState(0);
+  const likeClick = (): void => {
+    setLikeCount((prevStateLike) => {
+      return prevStateLike + 1;
+    });
+  };
 
-    const likeClick = () => {
-        setLikeCount((prevStateLike) => {
-            return prevStateLike + 1;
-        });
-    };
+  const dislikeClick = (): void => {
+    setDislikeCount((prevStateDislike) => {
+      return prevStateDislike + 1;
+    });
+  };
 
-    const dislikeClick = () => {
-        setDislikeCount((prevStateDislike) => {
-            return prevStateDislike + 1;
-        });
-    };
+  const resetResults = (): void => {
+    setLikeCount(0);
+    setDislikeCount(0);
+  };
 
-    const resetResults = () => {
-        setLikeCount(0);
-        setDislikeCount(0);
-    }
-
-    return (
-        <div className='feedback-wrapper'>
-            <div className="result-container">{like}</div>
-            <div className='button-wrapper'>
-                <Button
-                    name='Like'
-                    type="button"
-                    onClick={likeClick}
-                />
-            </div>
-            <div className='button-wrapper'>
-                <Button
-                    name='Dislike'
-                    type="button"
-                    onClick={dislikeClick}
-                />
-            </div>
-            <div className="result-container">{dislike}</div>
-            <div className='reset-button-wrapper'>
-                <Button
-                    name='Reset Results'
-                    type="button"
-                    onClick={resetResults}
-                />
-      </div> 
-        </div>
-    );
+  return (
+    <div className="feedback-wrapper">
+      <div className="result-container">{like}</div>
+      <div className="button-wrapper">
+        <Button name="Like" type="button" onClick={likeClick} />
+      </div>
+      <div className="button-wrapper">
+        <Button name="Dislike" type="button" onClick={dislikeClick} />
+      </div>
+      <div className="result-container">{dislike}</div>
+      <div className="reset-button-wrapper">
+        <Button name="Reset Results" type="button" onClick={resetResults} />
+      </div>
+    </div>
+  );
 }
 
-export default Feedback; 
+export default Feedback;
